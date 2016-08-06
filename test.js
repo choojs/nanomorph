1b0b36094f884491e9c8567814223766eba6009b
Yoshua Wuyts <yoshuawuyts@gmail.com>
2016-08-07 00:56:02 +0200

    scratch: init

--- test.js
+++ test.js
@@ -1,7 +1,22 @@
 const test = require('tape')
-const nanodiff = require('./')
+const html = require('bel')
+const nanomorph = require('./')
 
-test('should assert input types', function (t) {
-  t.plan(1)
-  t.throws(nanodiff)
+test('nanomorph', (t) => {
+  t.test('should assert input types', (t) => {
+    t.plan(2)
+    t.throws(nanomorph, /newTree/)
+    t.throws(nanomorph.bind(null, {}), /oldTree/)
+  })
+
+  t.test('should replace a node one level deep', (t) => {
+    t.plan(1)
+
+    const oldTree = html`<p>hello world</p>`
+    const newTree = html`<div>hello world</div>`
+
+    const res = nanomorph(newTree, oldTree)
+    const expected = '<div>hello world</div>'
+    t.equal(String(res), expected, 'result was expected')
+  })
 })
