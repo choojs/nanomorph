92a73d1a7e1f41a108a30d4badfda0df70df5d97
kj <kj@begin.com>
2017-01-09 08:52:54 -0800

    Adds preserving of new values. Fixes #27

--- test.js
+++ test.js
@@ -163,13 +163,23 @@ test('nanomorph', function (t) {
   })
 
   t.test('values', function (t) {
-    t.test('should copy over', function (t) {
+    t.test('should be copied to new tree when it has no value', function (t) {
       t.plan(1)
       var oldTree = html`<input type="text" />`
       oldTree.value = 'howdy'
       var newTree = html`<input type="text" />`
       var res = nanomorph(newTree, oldTree)
       t.equal(res.value, 'howdy')
+    })
+
+    t.test('should be copied to old tree when new tree has a value', function (t) {
+      t.plan(1)
+      var oldTree = html`<input type="text" />`
+      oldTree.value = 'howdy'
+      var newTree = html`<input type="text" />`
+      newTree.value = 'hi'
+      var res = nanomorph(newTree, oldTree)
+      t.equal(res.value, 'hi')
     })
   })
 
