0abde3212aa01607501d7dd5eaf2e8a7c87d98c7
Ronald Eddy Jr <ronald_eddy@yahoo.com>
2017-11-01 05:00:24 -0700

    Add test for Events (#90)

--- test/index.js
+++ test/index.js
@@ -2,11 +2,13 @@ var tape = require('tape')
 
 var abstractMorph = require('./diff')
 var nanomorph = require('../')
+var abstractMorphEvents = require('./events')
 
 if (!module.parent) {
   require('./fuzz')
   specificTests(nanomorph)
   abstractMorph(nanomorph)
+  abstractMorphEvents(nanomorph)
 } else {
   module.exports = abstractMorph
 }
